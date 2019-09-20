1a3eb0db8ef54fe5df4f09c02de60698b4255735
eecheng <yucheng871011@gmail.com>
2019-09-20 15:21:09 +0800

    - add indirect addressing example

--- workspace1.X/addressing_mode.asm
+++ workspace1.X/addressing_mode.asm
@@ -8,16 +8,42 @@ write MACRO Var, Address
 	movwf Address, 1
 	ENDM 	
     org 0x00
-
+    offset equ 0x13
 		
-
+ 
 
 start:
-    ;movlb 2
-    movlw 14
-    movwf 10, 1
-	write 0x39, 100
-	write 0x4e, 200
+	movlb 1
+	write 0x39, 0x0100
+	write 0x4e, 0x0101
+	write 0x4f, 0x0102
+	write 0x1f, 0x0103
+	write 0x0d, 0x0104
+	write 0x56, 0x0105
+	write 0x60, 0x0106
+	write 0x0d, 0x0107
+	write 0x52, 0x0108
+	write 0x4e, 0x0109
+	write 0x60, 0x010a
+ 	write 0x66, 0x010b
+
+	
+	;lfsr FSR0, 0x13
+	lfsr FSR1, 0x0100
+	lfsr FSR2, 0x0200
+
+loop:
+	movlw 0x0b
+	cpfsgt FSR1L;  
+	goto cal
+	goto exit
+cal:
+    	movf POSTINC1, 0 
+	addlw offset
+	movlb 2
+	movwf POSTINC2 
+	goto loop	
+exit:
 	nop
     
     end
