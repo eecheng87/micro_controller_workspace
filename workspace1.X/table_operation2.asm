27981cb632be54155d7964e9b88bfc41fed8fe0c
eecheng <yucheng871011@gmail.com>
2019-09-24 09:57:02 +0800

    - update

--- workspace1.X/table_operation2.asm
+++ workspace1.X/table_operation2.asm
@@ -1,3 +1,30 @@
-
+LIST p=18f4520
+#include<p18f4520.inc>
+    CONFIG OSC = INTIO67
+    CONFIG WDT = OFF
+    org 0x00
+    
+    start:
+    org 0x1000
+    
+    array    db    0x01,0x02,0x03,0x04,0x05,0x06,0x07 
+    ;array db 'a','b','c' ; store string 'a','b','c' into array at 0x1000 in 
+			 ; program mem
+			 
+    org 0x1100
+    movlw upper array
+    movwf TBLPTRU,0
+    movlw high array
+    movwf TBLPTRH,0
+    movlw low array
+    movwf TBLPTRL,0
+    tblrd*+		 ; check out TABLAT = 0x01
+    tblrd*+
+    tblrd*+
+    
+   
+    
+    nop
+    end
 
 
